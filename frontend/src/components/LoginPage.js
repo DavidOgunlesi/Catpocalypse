58cc55f0569e47657684ac2ef83d2864617dcadc
SimranSahota02 <simransahota0202@gmail.com>
2022-02-27 14:30:29 +0000

    Login page background and imports

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {render} from "react-dom";
+import {isMobile} from 'react-device-detect';
+import logo from '/static/images/logo.png';
+import cat from '/static/images/cat.png';
+import {Button, Grid, Typography} from "@material-ui/core";
+import Background from "./static/Background";
 
 export default function LoginPage(){
 
-    return (
-        <div>
-            <div>nuts</div>
-            <div></div>
-            <div></div>
-        </div>
-    );
+    if(isMobile){
+        return (
+            <Background 
+            gradient={false} 
+            primaryCol="#FEEAC2" 
+            outlineCol="#FFC992" 
+            outlineThickness={200} 
+            skew={-32}
+            backgroundCol="#FFF59D"
+            >
+            </Background>
+        );
+    }else {
+
+    }
+    
     
 }
