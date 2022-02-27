1250e56c37f4133f6cf44e23d7ee12e08a06205a
NishilAmin1213 <na510@exeter.ac.uk>
2022-02-27 16:36:31 +0000

    Fixed Bug from removing example code, made migrations to view database

--- backend/models.py
+++ backend/models.py
@@ -29,8 +29,18 @@ def rand_val():
     """
     return randint(50,100)
 
+def exampleFunction():
+    return "This is a Random String"
+
 
 # Create your models here.
+class ExampleModel(models.Model):
+    # define fields we want for each room
+    code = models.CharField(max_length=8, default=exampleFunction, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Cats(models.Model):
     # define fields for the Cats table in the database
