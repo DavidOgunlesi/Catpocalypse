e091fbd931f4db012444d6903e5d86fb61ac712d
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2022-02-28 17:05:17 +0000

    Fixed logo on desktop warning page
    Logo scales properly with screen

--- frontend/src/components/DesktopWarningPage.js
+++ frontend/src/components/DesktopWarningPage.js
@@ -3,28 +3,27 @@ import logo from '/static/images/logo.png';
 import cat from '/static/images/cat.png';
 import {Button, Typography} from "@material-ui/core";
 import Background from "./static/Background";
-
 export default function DesktopWarningPage(){
 
     return (
-            <Background 
-            gradient={false} 
-            primaryCol="#FEEAC2" 
-            backgroundCol="#FFF59D"
-            >
-                <Button 
-                variant="outline" 
-                style={{ width:"100%", height: "100%", position:"absolute"}}
-                />
-                <img src={logo} style={{ width:"100%", height: "70%", position:"center"}} alt="Logo" />
-                <Typography style={{backgroundColor:"black", color:"white", height:"10%", 
-                display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    Desktop Browser currently not supported<br/>Please open with a mobile device to play!
-                </Typography>
-                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <img src={cat} alt="Cat"/>
-                </div>
-            </Background>
-            );
+        <Background 
+        gradient={false} 
+        primaryCol="#FEEAC2" 
+        backgroundCol="#FFF59D"
+        >
+            <Button 
+            variant="outline" 
+            style={{ width:"100%", height: "100%", position:"absolute"}}
+            />
+            <img src={logo} className="desktopLogo" alt="Logo" />
+            <Typography style={{backgroundColor:"black", color:"white", height:"10%", 
+            display: "flex", justifyContent: "center", alignItems: "center"}}>
+                Desktop Browser currently not supported<br/>Please open with a mobile device to play!
+            </Typography>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <img src={cat} alt="Cat"/>
+            </div>
+        </Background>
+        );
     
 }
