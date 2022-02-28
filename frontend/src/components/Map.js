30f9e815a00c6f434ae074246e1e2305291d4c58
Inshallah <inshallah295@gmail.com>
2022-02-28 21:27:52 +0000

    Added map styling
    Changed imports to a different library, reworked Map component and added new styling from google cloud-based map styling.

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+//import ReactDOM from "react-dom";
+import React from "react";
+import {
+  GoogleMap,
+  LoadScript
+} from "@react-google-maps/api";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const lib = ["places"];
+const id = ["64f4173bca5b9f91"]
+const key = "AIzaSyDv-LEbSc-bYO2UUkBXmiJ-l846ItAKhL4&map_id=64f4173bca5b9f91";
+const defaultLocation = { lat: 50.736603, lng: -3.533233};
 
-export default class Map extends Component {
-  static defaultProps = {
-    center: {
-      lat: 50.736603,
-      lng: -3.533233
-    },
-    zoom: 19
-  };
-
+export default class Map extends React.Component {
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyDv-LEbSc-bYO2UUkBXmiJ-l846ItAKhL4"}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={50.736603}
-            lng={-3.533233}
-            text="My Marker"
-          />
-        </GoogleMapReact>
+      <div>
+       <LoadScript googleMapsApiKey={key} libraries={lib} mapIds={id} >
+        <GoogleMap
+          center={defaultLocation}
+          zoom={19}
+          options={{ mapId: "64f4173bca5b9f91" }}
+          mapContainerStyle={{ height: "100vh", width: "100%" }}
+        />
+        </LoadScript>
       </div>
     );
   }
