f595755302724a5df4607af42213574aebbe9424
Jason Jashari <13jasonja@gmail.com>
2022-03-01 13:24:59 +0000

    removed all example code

--- backend/serializers.py
+++ backend/serializers.py
@@ -2,11 +2,7 @@ from rest_framework import serializers
 from rest_framework.authtoken.views import Token
 
 # Import Models here
-from .models import ExampleModel
 from django.contrib.auth.models import User
-
-
-
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -23,23 +19,6 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
-
-
-
-
-# Define Serializers here
-class ExampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExampleModel
-        # 'id' is primary key, every model has an "invisible" id field
-        fields = ('id', 'code', 'host', 'guest_can_pause', 
-                  'votes_to_skip', 'created_at')
-        
-class ExampleSerializer2(serializers.ModelSerializer):
-    class Meta:
-        model = ExampleModel
-        # 'id' is primary key, every model has an "invisible" id field
-        fields = ('code', 'created_at')
         
 '''class GetCatSerialiser(serializers.ModelSerializer):
     class Meta:
