6633dc22de48bf19dc03fbd55b4f41dd8452af04
JasonJashari <73113041+JasonJashari@users.noreply.github.com>
2022-03-02 03:26:15 +0000

    Redacted

--- catpocalypse/settings.py
+++ catpocalypse/settings.py
@@ -142,5 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gsep.testsender@gmail.com'
-EMAIL_HOST_PASSWORD = 'TestSender123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
