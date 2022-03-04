6b288f2771620f4f4b63156381d82f2ec1e9378a
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2022-03-04 17:48:18 +0000

    Cats bounce randomly and no longer in a weird unison

--- frontend/src/components/static/MapMarkers/CatMapMarker.js
+++ frontend/src/components/static/MapMarkers/CatMapMarker.js
@@ -2,20 +2,33 @@
  * Returns Cat markers on the app on the Map View when the user is playing the game
  * The imports which are required for this page to run which includes packages from React and other files which exist.
  */
-import React from "react";
+import React, {Component} from "react";
 import Cat from "/static/images/cat.png";
+import {getRandomRange} from '/src/util/math.js'
 
 /**
  * Main Function of CatMapMarker.js
  * @returns The cat image which bounces on the interactive map
  */
-export default function CatMapMarker({
-    size=50
-}){
+export default class CatMapMarker extends Component{
+    constructor(props) {
+        super(props);
+        var randomBounceTime = getRandomRange(0.3,1);
+        var style = {
+            animation: `bounce ${randomBounceTime}s infinite alternate`,
+            webkitAnimation: `bounce ${randomBounceTime}s infinite alternate`
+        }
+        this.state = {
+            style: style,
+          };
+        console.log("Constructed");
+      }
+    render() {
     return (
         <div class="markerImg">
-            <img class="bounce" width={size} src={Cat}/>
+            <img width={this.props.size} src={Cat} style={this.state.style}/>
         </div>
     );
+}
     
 }
