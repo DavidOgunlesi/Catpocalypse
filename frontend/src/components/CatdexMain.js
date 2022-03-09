8a4dc8031153df4bfe0463600e4954b9baf3f2e8
SimranSahota02 <simransahota0202@gmail.com>
2022-03-09 14:26:08 +0000

    Catdex background colour palette

--- frontend/src/components/CatdexMain.js
+++ frontend/src/components/CatdexMain.js
@@ -1,21 +1,28 @@
-import React, {useEffect, useState} from "react";
-import ModalWindow from "./dynamic/ModalWindow";
-import warningCat from '/static/images/warningCat.png';
-import { geolocated } from "react-geolocated";
-import GoogleMapReact from 'google-map-react';
+import React, {useState, useEffect} from "react";
+import {render} from "react-dom";
+import {isMobile} from 'react-device-detect';
+import logo from '/static/images/logo.png';
+import cat from '/static/images/cat.png';
+import {Button, Grid, Typography, InputAdornment} from "@material-ui/core";
+import FallingCat from "./dynamic/FallingCat";
+import {getRandomRange} from '/src/util/math.js';
 import Background from "./static/Background";
-import HorizontalCompass from "./dynamic/HorizontalCompass";
-import { useDrag, useGesture } from '@use-gesture/react'
+import {Link} from "react-router-dom";
+import LoginPage from "./LoginPage";
+import {GoogleLogin} from 'react-google-login';
+import GoogleLogo from '/static/images/GoogleLogo.png';
+import ModalWindow from "./dynamic/ModalWindow";
+import ChoiceModalWindow from "./dynamic/ChoiceModalWindow";
 
 export default function CatdexEntry(){
 	if (isMobile){
 		return(<Background 
         gradient={false} 
-        primaryCol="#FEEAC2" 
-        outlineCol="#FFC992" 
+        primaryCol="#E2FADD" 
+        outlineCol="#9EE6C9" 
         outlineThickness={200} 
         skew={-32}
-        backgroundCol="#FFF59D"
+        backgroundCol="#B8FCF3"
         ></Background>);
 		}
 	else{
