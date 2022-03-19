b6fd06d05a040090b32642d31b21f8399f185768
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2022-03-19 17:46:15 +0000

    Added anchor prop to Overlay UI children to specify placement on screen

--- frontend/src/components/dynamic/OverlayUI.js
+++ frontend/src/components/dynamic/OverlayUI.js
@@ -14,91 +14,130 @@ import { makeStyles } from "@material-ui/core";
    * Styling for UI
    */
   const style = {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    //pointerEvents: 'none',
-    background: 'transparent',
-    textAlign: 'center',
-    zIndex: 10000
+	position: 'absolute',
+	width: '100%',
+	height: '100%',
+	//pointerEvents: 'none',
+	background: 'transparent',
+	textAlign: 'center',
+	zIndex: 10000
   };
 
   const useDefStyles = makeStyles((theme) => ({
-    small: {
-      '& svg': {
-        fontSize: 25
-      }
-    },
-    medium: {
-      '& svg': {
-        fontSize: 50
-      }
-    },
-    large: {
-      '& svg': {
-        fontSize: 75
-      }
-    },
-    huge: {
-      '& svg': {
-        fontSize: 100
-      }
-    }
+	small: {
+	  '& svg': {
+		fontSize: 25
+	  }
+	},
+	medium: {
+	  '& svg': {
+		fontSize: 50
+	  }
+	},
+	large: {
+	  '& svg': {
+		fontSize: 75
+	  }
+	},
+	huge: {
+	  '& svg': {
+		fontSize: 100
+	  }
+	}
   }));
 
 /**
  * 
  */
 export default function OverayUI({
-    children
+	children
 }){
-    const defClasses = useDefStyles();
+	const defClasses = useDefStyles();
 
-    const childrenWithProps = React.Children.map(children, child => {
-        // Checking isValidElement is the safe way and avoids a typescript
-        // error too.
-        if (React.isValidElement(child)) {
-            var childrenStyle = {
-                position: 'relative',
-                float: child.props.float,
-                top: child.props.y,
-                left: child.props.x,
-            }
-            // Make custom style class to resize icon if applicable
-            var useStyles = makeStyles((_) => ({
-                size: {
-                    '& svg': {
-                    fontSize: child.props.overrideSize
-                    }
-                }
-            }));
-            var classes = useStyles();
-            var sizeClass = classes.size
-            switch (child.props.size) {
-                case "small":
-                    sizeClass = defClasses.small
-                    break;
-                case "medium":
-                    sizeClass = defClasses.medium
-                    break;
-                case "large":
-                    sizeClass = defClasses.large
-                    break;
-                case "huge":
-                    sizeClass = defClasses.huge
-                    break;
-                default:
-                    break;
-            }
-            return React.cloneElement(child, { className: sizeClass, style: childrenStyle });
-        }
-        return child;
-      });
+	const childrenWithProps = React.Children.map(children, child => {
+		// Checking isValidElement is the safe way and avoids a typescript
+		// error too.
+		if (React.isValidElement(child)) {
+		  var childrenStyle = {}
+		  if (child.props.anchor == "left"){
+				childrenStyle = {
+					position: 'absolute',
+					top: child.props.y,
+					left: child.props.x
+				}
+		  	}else if (child.props.anchor == "top right"){
+				childrenStyle = {
+					position: 'absolute',
+					top: child.props.y,
+					right: child.props.x
+				}
+			}else if (child.props.anchor == "top middle"){
+				childrenStyle = {
+					position: 'absolute',
+					top: child.props.y,
+					left: child.props.x
+				}
+			} else if (child.props.anchor == "top bottom"){
+				childrenStyle = {
+					position: 'absolute',
+					left: child.props.x,
+					bottom: child.props.y
+				}
+			}else if (child.props.anchor == "bottom right"){
+				childrenStyle = {
+					position: 'absolute',
+					right: child.props.x,
+					bottom: child.props.y
+				}
+			}else if (child.props.anchor == "bottom left"){
+				childrenStyle = {
+					position: 'absolute',
+					left: child.props.x,
+					bottom: child.props.y
+				}
+			}else{
+				childrenStyle = {
+					position: 'absolute',
+					right: child.props.x,
+					top: child.props.y
+				}
+			}
+		  
+			// Make custom style class to resize icon if applicable
+			var useStyles = makeStyles((_) => ({
+				size: {
+					'& svg': {
+					fontSize: child.props.overrideSize
+					}
+				}
+			}));
+			var classes = useStyles();
+			var sizeClass = classes.size
+			switch (child.props.size) {
+				case "small":
+					sizeClass = defClasses.small
+					break;
+				case "medium":
+					sizeClass = defClasses.medium
+					break;
+				case "large":
+					sizeClass = defClasses.large
+					break;
+				case "huge":
+					sizeClass = defClasses.huge
+					break;
+				default:
+					break;
+			}
+			return React.cloneElement(child, { className: sizeClass, style: childrenStyle });
+		}
+		return child;
+	  });
 
-    return (
-        <Box sx={style}>
-            {childrenWithProps}
-        </Box>
-      );
-    
+	return (
+		<Box sx={style}>
+			{childrenWithProps}
+		</Box>
+	  );
+	
 }
