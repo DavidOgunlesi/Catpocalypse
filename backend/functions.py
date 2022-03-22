9a495a1abfb8e72282234dcbd1af2dab317f19ec
NishilAmin1213 <na510@exeter.ac.uk>
2022-03-22 23:03:06 +0000

    saved p1 and p2

--- backend/functions.py
+++ backend/functions.py
@@ -30,6 +30,8 @@ def get_free_players():
 
     player1.is_available = False
     player2.is_available = False
+    player1.save()
+    player2.save()
 
     # get a random cat from the Cats table
     rand_cat = Cats.objects.order_by('?').first() 
