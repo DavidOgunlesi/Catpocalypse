35c4e88ee2c82f5c2dd0f99bc843a05f5320095a
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2022-03-23 18:04:24 +0000

    Added log out button to settings page

--- frontend/src/components/SettingsPage.js
+++ frontend/src/components/SettingsPage.js
@@ -2,15 +2,38 @@
  * A set template used to check if the page is working or not.
  * Imports React from the React Package
  */
- import React from "react";
+import React from "react";
+import { InputAdornment, Button, Typography } from "@material-ui/core";
+import ExitToApp from "@material-ui/icons/ExitToApp";
 
  export default function SettingsPage(){
  
+    const logout = () => {
+        fetch('/api/logout')
+		.then(response => response.json())
+		.then(data => {
+            console.log(data);
+            window.location.reload();
+        })
+    }
      /**
       * Returns Hello World!
       */
      return (
-         <div>Hello World!</div>
+        <Button 
+        color = 'primary' 
+        variant = "contained"
+        size = "large" 
+        style = {{ borderRadius: 50 }}
+        fullWidth = {true}
+        onClick={()=>logout()}
+        startIcon={
+            <InputAdornment position="start">
+                 <ExitToApp />
+            </InputAdornment>}
+        >
+            <Typography variant="h4" component="h4">Logout</Typography>
+         </Button>
      );
      
  }
