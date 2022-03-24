387bb1c3b4815d1df035ae21505a33ee885a707d
Inshallah <inshallah295@gmail.com>
2022-03-24 10:56:03 +0000

    Added female/male icons and added to catdex profile

--- frontend/src/components/subpages/CatdexProfile.js
+++ frontend/src/components/subpages/CatdexProfile.js
@@ -4,6 +4,7 @@
  */
  import React from "react";
  import {Button, Grid, Typography, InputAdornment, Item, IconButton} from "@material-ui/core";
+ import Box from '@material-ui/core/Box';
  import MissingCat from "/static/images/cats/undefined.png";
 
  export default function CatdexProfile({cat}){
@@ -22,76 +23,62 @@
                  break;
             case 4:
                 return 12;
-                break;
             case 5:
                 return 10;
-                break;
             case 6:
                 return 5;
-                break;
             case 7:
                 return 7;
-                break;
             case 10:
                 return 11;
-                break;
             case 11:
                 return 11;
-                break;
             case 14:
                 return 9;
-                break;
                 //
             case 15:
                 return 12;
-                break;
             case 16:
                 return 12;
-                break;
             case 17:
                 return 12;
-                break;
             case 18:
                 return 10;
-                break;
             case 19:
                 return 12;
-                break;
             case 20:
                 return 8;
-                break;
             case 21:
                 return 11;
-                break;
-            //
             case 22:
                 return 10;
-                break;
             case 23:
                 return 10;
-                break;
             case 24:
                 return 12;
-                break;
             case 26:
                 return 10;
-                break;
             case 27:
                 return 12;
-                break;
             case 28:
                 return 7;
-                break;
             default:
                 return 8;
-                break;
         }
     }
 
-     /**
-      * Returns Hello World!
-      */
-     return (
+    function setGender() {
+        if (cat.sex == 'male') {
+            return '/static/icons/male.svg';
+        } else {
+            return '/static/icons/female.svg';
+        }
+    }
+
+    /**
+     * Returns Hello World!
+     */
+    return (
         <div  style = {{opacity:1}}>
         <Grid container spacing={2} style={{padding:20}} justifyContent="center" alignItems="center">
             <Grid item xs={12} align="center">
@@ -100,7 +87,11 @@
             <Grid item xs={setImageSize()} align="center">
                 <img src={`/static/images/cats/${cat.id}.png`}/>
             </Grid>
+            <Box width="100%"/>
+            <Grid item xs={2} align="center">
+                <img src={setGender()}/>
+            </Grid>
         </Grid>
         </div>
-     );    
+    );    
 } 
